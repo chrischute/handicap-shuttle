d2733976f687ed677ed8b87acd7b00e322d3346b
chrischute <chutechristopher@gmail.com>
2016-10-31 16:27:27 -0400

    Pushing to GitHub.

--- Wheels/MainPageViewController.swift
+++ Wheels/MainPageViewController.swift
@@ -9,21 +9,61 @@
 import UIKit
 
 class MainPageViewController: UIPageViewController, UIPageViewControllerDataSource {
-
+    private var pages: [UIViewController] =
+        [MainPageViewController.pageVC(with: StoryboardConstants.queueViewControllerId),
+         MainPageViewController.pageVC(with: StoryboardConstants.onDemandViewControllerId),
+         MainPageViewController.pageVC(with: StoryboardConstants.planAheadViewControllerId)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        dataSource = self
+        if let firstPageToDisplay = pages[1] as? OnDemandViewController {
+            setViewControllers([firstPageToDisplay],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func viewController(at index: Int) -> ContentViewController {
-        if (index >= self.pageTitles.count) {
-            
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    /* UIPageViewControllerDataSource Protocol */
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let vcIndex = pages.index(of: viewController) {
+            if vcIndex > 0 {
+                return pages[vcIndex - 1]
+            }
         }
+        
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        if let vcIndex = pages.index(of: viewController) {
+            if vcIndex < pages.count - 1 {
+                return pages[vcIndex + 1]
+            }
+        }
+        
+        return nil
+    }
+    
+    private class func pageVC(with id: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: id)
     }
 }
